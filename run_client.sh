f11275ddafac2b15d1c986ebde1a75bf7a85a4e3
PeggyChen19 <peggy10191019@gmail.com>
2025-12-03 08:35:45 +0000

    add mode argu to client script

--- run_client.sh
+++ run_client.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 json_file=$1
+mode=$2
 
 python sim/client_simulator.py \
   --max-concurrent-requests 500 \
@@ -9,6 +10,6 @@ python sim/client_simulator.py \
   --server-url http://127.0.0.1:8000 \
   --model-name facebook/opt-125m \
   --backend-model-name trace-sim \
-  --mode single \
+  --mode $mode \
   --no-use-timestamps
 
